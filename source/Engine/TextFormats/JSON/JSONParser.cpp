9332f6925415747e97360f0460f4a019757a6346
vvvvvvitor <vitorgabrielgomesro9090@gmail.com>
2026-08-04 23:15:24 -0300

    First commit

---
+++ source/Engine/TextFormats/JSON/JSONParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vitorgabrielgomesrodrigues on 04/08/2026.
+//
+
+#include "../XML/JSONParser.h"
---
+++ source/Engine/TextFormats/JSON/JSONParser.h
@@ -0,0 +1,17 @@
+//
+// Created by vitorgabrielgomesrodrigues on 04/08/2026.
+//
+
+#ifndef HATCHGAMEENGINE_JSONPARSER_H
+#define HATCHGAMEENGINE_JSONPARSER_H
+
+struct JSONToken {
+    Stream* base_stream
+};
+
+class JSONParser {
+    static
+};
+
+
+#endif //HATCHGAMEENGINE_JSONPARSER_H
